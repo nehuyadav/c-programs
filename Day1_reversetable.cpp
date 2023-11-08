9baa1da1f4daa119057675931adfad591129c9b3
nehuyadav <146457490+nehuyadav@users.noreply.github.com>
2023-11-08 16:46:20 +0530

    Add files via upload

---
+++ Day1_reversetable.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	 int i,n;
+	 printf("enter a number for reverse order table print\n");
+	 scanf("%d",&n);
+	 i=10;
+	 while(i>=1){
+	 	  printf("%d\t",i*n);
+	 	  i--; 
+	 } 
+}
+
